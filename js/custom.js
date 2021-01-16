bc94429f496017ff306fa57b67ad5030b40dbf2e
Siben Nayak <siben.nayak@gmail.com>
2021-01-16 17:46:34 +0530

    Added registration form

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+function submitToAPI(e) {
+   e.preventDefault();
+   var URL = "https://v4v5o1d08k.execute-api.ap-south-1.amazonaws.com/01/registration";
+   var email = $("#email").val();
+   console.log(email);
+   var data = {
+      email: email
+   };
+   $.ajax({
+      type: "POST",
+      url: URL,
+      dataType: "json",
+      crossDomain: "true",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(data),
+      success: function () {
+         location.reload();
+      },
+      error: function () {}
+   });
+}
